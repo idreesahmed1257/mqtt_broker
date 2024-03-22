85c63fb23f79d647f6076baf9a89476f1c83c4e9
idreesahmed697 <idrees.ahmed@techvention.ae>
2024-03-22 19:54:22 +0500

    asd

--- aedes_broker/server.js
+++ aedes_broker/server.js
@@ -1,36 +1,17 @@
-// const aedes = require('aedes')();
-// const server = require('net').createServer(aedes.handle);
-// const port = 5007;
+const aedes = require('aedes')();
+const server = require('net').createServer(aedes.handle);
+const port = 5007;
 
-// server.listen(port  , function () {
-//     console.log('Aedes MQTT broker running on port:', port);
-// });
-
-// // Handle client connection
-// aedes.on('client', function (client) {
-//     console.log('Client Connected:', client.id);
-// });
-
-// // Handle client disconnection
-// aedes.on('clientDisconnect', function (client) {
-//     console.log('Client Disconnected:', client.id);
-// });
-
-
-const fastify = require('fastify')({ logger: true });
-
-fastify.get('/', async (request, reply) => {
-    return { hello : 'MQTT'}
+server.listen(port  , function () {
+    console.log('Aedes MQTT broker running on port:', port);
 });
 
-const start = async () => {
-    try {
-        await fastify.listen({ port: 5060 });
-        console.log(`Fastify server running on port ${5060}`);
-    } catch (err) {
-        fastify.log.error(err);
-        process.exit(1);
-    }
-};
+// Handle client connection
+aedes.on('client', function (client) {
+    console.log('Client Connected:', client.id);
+});
 
-start();
+// Handle client disconnection
+aedes.on('clientDisconnect', function (client) {
+    console.log('Client Disconnected:', client.id);
+});
