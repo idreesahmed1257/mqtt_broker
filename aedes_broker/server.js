f8bf4650c1b9473c3ddbd02e7bb9ed58400284a3
idreesahmed697 <idrees.ahmed@techvention.ae>
2024-03-22 19:29:34 +0500

    dsad

--- aedes_broker/server.js
+++ aedes_broker/server.js
@@ -15,3 +15,22 @@ aedes.on('client', function (client) {
 aedes.on('clientDisconnect', function (client) {
     console.log('Client Disconnected:', client.id);
 });
+
+
+const fastify = require('fastify')({ logger: true });
+
+fastify.get('/', async (request, reply) => {
+    return { hello : 'MQTT'}
+});
+
+const start = async () => {
+    try {
+        await fastify.listen({ port: 5060 });
+        console.log(`Fastify server running on port ${5060}`);
+    } catch (err) {
+        fastify.log.error(err);
+        process.exit(1);
+    }
+};
+
+start();
